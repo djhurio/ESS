23091824b5d687dc011ea223c2c85f0eceabb2b9
Mārtiņš Liberts <martins.liberts@gmail.com>
2018-04-02 11:50:38 +0300

    deff_p

--- Rcode/deff_p.R
+++ Rcode/deff_p.R
@@ -7,7 +7,10 @@ deff_p <- function(N) {
 }
 
 N_example <- c(25, 52, 15, 5, 2, 1)
+sum(N_example * seq_along(N_example))
+
 N_LV <- c(552310, 303668, 103073, 29391, 4653, 773, 172, 55, 15, 5, 5, 2)
+sum(N_LV * seq_along(N_LV))
 
 deff_p(N_example)
 deff_p(N_LV)

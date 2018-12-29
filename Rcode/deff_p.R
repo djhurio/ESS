18c556476f978f146c433f25c4fc14c01d482925
Mārtiņš Liberts <martins.liberts@gmail.com>
2018-12-29 22:40:20 +0200

    deff_p simulation

--- Rcode/deff_p.R
+++ Rcode/deff_p.R
@@ -1,5 +1,4 @@
 # deff_p
-
 deff_p <- function(N) {
   M <- seq_along(N)
   w <- N / sum(N)
@@ -14,3 +13,35 @@ sum(N_LV * seq_along(N_LV))
 
 deff_p(N_example)
 deff_p(N_LV)
+
+
+
+# Poisson Sampling
+
+require(data.table)
+require(sampling)
+
+data(belgianmunicipalities)
+setDT(belgianmunicipalities)
+
+# deff_p
+deff_p <- function(x) {
+  M <- x
+  w <- 1 / lengths(x)
+  sum(w * M ^ 2) / sum(w * M) ^ 2
+}
+
+# Sample size
+n <- 200
+
+# Probability of selection
+belgianmunicipalities[, pik := inclusionprobabilities(Tot, n)]
+
+# Select a sample
+belgianmunicipalities[, s := UPpoisson(pik)]
+
+# Design Weight
+belgianmunicipalities[, dw := 1 / pik]
+
+belgianmunicipalities[, deff_p(dw)]
+belgianmunicipalities[s == 1, deff_p(dw)]
